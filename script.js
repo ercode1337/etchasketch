4bdd7f8f22d5bdb48f971aadd0d5bebef08a1418
ercode1337 <eric.melin@outlook.com>
2023-03-22 23:37:41 +0200

    ok

--- script.js
+++ script.js
@@ -1,13 +1,77 @@
-const container = document.getElementById("container");
+const currentGridSize = '';
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+const slider = document.getElementById("slider");
+slider.addEventListener("input", (e) => {
+	const num = parseInt(e.target.value);
+	if (num) {
+		if (num > currentGridSize) {
+			createGrid(num);
+		} else {
+			destroyGrid(num);
+		}
+		currentGridSize = num;
+	}
+});
 
-makeRows(16, 16);
+function createGrid(num) {
+  const container = document.getElementById("container");		
+  container.innerText = '';
+  for (let i = 0; i < num*num; i++) {
+    let gridBox = document.createElement("div");
+    gridBox.setAttribute("class", "grid-box");
+    container.appendChild(gridBox);
+  }
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+  const gridBoxes = document.querySelectorAll('.grid-box');
+
+
+  gridBoxes.forEach(gridBox => {
+    gridBox.addEventListener('click', function(e) {
+      e.target.style.backgroundColor = 'black';
+    });
+    gridBox.addEventListener('mouseover', function(e) {
+      if (e.buttons === 1) {
+        e.target.style.backgroundColor = 'black';
+      }
+    });
+  });
+
+  const eraserBtn = document.getElementById("eraser");
+
+  eraserBtn.addEventListener('click', () => {
+    gridBoxes.forEach(gridBox => {
+      gridBox.addEventListener('click', function(e) {
+        e.target.style.backgroundColor = 'white';
+      });
+      gridBox.addEventListener('mouseover', function(e) {
+        if (e.buttons === 1) {
+        e.target.style.backgroundColor = 'white';
+      }
+    });
+    gridBox.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = 'white';
+      });
+  });
+});
+
+const blackBtn = document.getElementById("black")
+blackBtn.addEventListener('click', () => {
+  gridBoxes.forEach(gridBox => {
+    gridBox.addEventListener('click', function(e) {
+      e.target.style.backgroundColor = 'black';
+    });
+    gridBox.addEventListener('mouseover', function(e) {
+      if (e.buttons === 1) {
+      e.target.style.backgroundColor = 'black';
+    }
+  });
+});
+});
+}
+
+createGrid(16);
+
+const title = document.getElementById("title");
+title.innerText = 'Etch-a-Sketch!';
